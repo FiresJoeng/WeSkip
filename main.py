02b0314486269df93c18ac78c76c78044144379e
FiresJoeng <iFiresGamer@iCloud.com>
2025-01-22 22:54:41 +0800

    update codes

--- main.py
+++ main.py
@@ -1,33 +1,27 @@
 from ctypes import *
 from fetch_pid import PID
 
-# 加载动态链接库
 dll = windll.LoadLibrary("HookDll.dll")
 
-# 获取 DLL 中的函数
+try:
+    print(f'已查找到WeChatAppEx.exe: {PID}')
+except Exception as e:
+    print(f"未查找到WeChatAppEx.exe: {e}")
+
 StartHook = getattr(dll, "StartHook")
 setSpeed = getattr(dll, "setSpeed")
 StopHook = getattr(dll, "StopHook")
 
 def SPEEDx10():
-    """
-    将指定进程变速为 10 倍速
-    """
     try:
-        print(f"Setting process {PID} to 10x speed.")
-        StartHook(c_ulong(PID))  # 启用变速
-        setSpeed(c_double(10.0))  # 设置为 10 倍速
-        print(f"Process {PID} is now running at 10x speed.")
+        StartHook(c_ulong(PID))
+        setSpeed(c_double(10.0))
     except Exception as e:
-        print(f"Failed to set process {PID} to 10x speed: {e}")
+        print(f"错误: {e}")
 
 def SPEEDx1():
-    """
-    将指定进程变速恢复为 1 倍速或停止变速
-    """
     try:
-        print(f"Stopping speed modification for process {PID}.")
-        StopHook(c_ulong(PID))  # 停止变速
-        print(f"Process {PID} is now running at normal speed.")
+        setSpeed(c_double(1))
+        StopHook(c_ulong(PID))
     except Exception as e:
-        print(f"Failed to stop speed modification for process {PID}: {e}")
+        print(f"错误: {e}")
