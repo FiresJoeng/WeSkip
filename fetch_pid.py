0f32f2163ae7d1791839776a1f248afdf19c13ce
FiresJoeng <iFiresGamer@iCloud.com>
2025-01-22 23:09:44 +0800

    update codes

--- fetch_pid.py
+++ fetch_pid.py
@@ -2,16 +2,15 @@ import psutil
 
 process_name = "WeChatAppEx.exe"
 PID = None
+RAM = 0
 
 for process in psutil.process_iter(['pid', 'name', 'memory_info']):
     try:
         if process.info['name'] == process_name:
-            PID = process.info['pid']
-            break
-    except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-        exit()
-
-if PID:
-    print(f"已确定{process_name}的PID: {PID}")
-else:
-    exit(f"未找到{process_name}!")
+            ram_usage = process.info['memory_info'].rss  # 获取常驻内存大小（字节）
+            if ram_usage > RAM:
+                RAM = ram_usage
+                PID = process.info['pid']
+    except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess) as E:
+        print("Error 1!")
+        exit(f"未找到{process_name}, 请先运行微信小程序, 并且给予本程序足够的权限, 然后再试一次! ")
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from ctypes import *
-from fetch_pid import PID
+from fetch_pid import PID, process_name, RAM
+
+if PID:
+    print(f"已确定{process_name}的PID: {PID}, RAM使用: {RAM / (1024 * 1024):.2f} MB.")
+else:
+    print("Error 2!")
+    exit(f"未找到{process_name}, 请先运行微信小程序, 并且给予本程序足够的权限, 然后再试一次! ")
 
 dll = windll.LoadLibrary("HookDll.dll")
-
-try:
-    print(f'已查找到WeChatAppEx.exe: {PID}')
-except Exception as e:
-    print(f"未查找到WeChatAppEx.exe: {e}")
 
 StartHook = getattr(dll, "StartHook")
 setSpeed = getattr(dll, "setSpeed")
