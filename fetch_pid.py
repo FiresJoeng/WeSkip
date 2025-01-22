ba47368a8d19c528e78b5e14efd13c5f8c7e0a45
FiresJoeng <iFiresGamer@iCloud.com>
2025-01-22 23:00:40 +0800

    Update fetch_pid.py

--- fetch_pid.py
+++ fetch_pid.py
@@ -1,25 +1,17 @@
 import psutil
 
-def get_max_ram_pid(process_name):
-    max_ram = 0
-    max_pid = None
+process_name = "WeChatAppEx.exe"
+PID = None
 
-    for process in psutil.process_iter(['pid', 'name', 'memory_info']):
-        try:
-            if process.info['name'] == process_name:
-                ram_usage = process.info['memory_info'].rss  # 获取进程的常驻内存大小（以字节为单位）
-                if ram_usage > max_ram:
-                    max_ram = ram_usage
-                    max_pid = process.info['pid']
-        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-            continue
+for process in psutil.process_iter(['pid', 'name', 'memory_info']):
+    try:
+        if process.info['name'] == process_name:
+            PID = process.info['pid']
+            break
+    except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+        exit()
 
-    return max_pid, max_ram
-
-if __name__ == "__main__":
-    process_name = "WeChatAppEx.exe"
-    pid, ram = get_max_ram_pid(process_name)
-    if pid:
-        print(f"PID: {pid}, RAM Usage: {ram / (1024 * 1024):.2f} MB")
-    else:
-        print(f"No process found with name '{process_name}'.")
+if PID:
+    print(f"已确定{process_name}的PID: {PID}")
+else:
+    exit(f"未找到'{process_name}'.")
